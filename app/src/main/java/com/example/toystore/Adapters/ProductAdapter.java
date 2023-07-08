fb962cf82385e7dea526626754c8e80c54857ddb
Moteyabai <134784925+Moteyabai@users.noreply.github.com>
2023-07-09 05:21:13 +0700

    Xong recycleview

---
+++ app/src/main/java/com/example/toystore/Adapters/ProductAdapter.java
@@ -0,0 +1,88 @@
+package com.example.toystore.Adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.toystore.Models.Product;
+import com.example.toystore.R;
+
+import java.util.ArrayList;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+
+    ArrayList<Product> productList;
+    SQLiteDatabase db;
+
+    public ProductAdapter(ArrayList<Product> productList) {this.productList = productList;}
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_layout,parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductAdapter.ViewHolder holder, int position) {
+
+        final Product product = productList.get(position);
+        holder.proName.setText(product.getProductName());
+
+        //Edit
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PopupMenu popupMenu = new PopupMenu(v.getContext(), holder.edit);
+                popupMenu.inflate(R.menu.edit_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int id = item.getItemId();
+                        if(id == R.id.update_menu){
+
+                        }else if(id == R.id.delete_menu){
+
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView proName;
+        ImageButton edit;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            proName = (TextView) itemView.findViewById(R.id.txtProName);
+            edit = (ImageButton) itemView.findViewById(R.id.options);
+        }
+    }
+}
--- app/src/main/java/com/example/toystore/Admin.java
+++ app/src/main/java/com/example/toystore/Admin.java
@@ -1,20 +1,41 @@
 package com.example.toystore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.toystore.Adapters.ProductAdapter;
+import com.example.toystore.Models.Product;
+
+import java.util.ArrayList;
+
 public class Admin extends AppCompatActivity {
     Button logout;
+    ArrayList<Product> proList;
+    DBHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
         logout = findViewById(R.id.logout);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv_product);
+
+        proList = new ArrayList<>();
+        proList.add(new Product(1, "Toy", 30000, "something"));
+
+        ProductAdapter adapter = new ProductAdapter(proList);
+
+        rv.setAdapter(adapter);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
